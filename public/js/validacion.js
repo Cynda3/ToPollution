04fa36dcf27f3cb02593887d04f80988931fed90
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-12-19 13:29:19 +0100

    Comments on validation js

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
+    //Hide all error messages
     $('#nameError').hide();
     $('#emailError').hide();
     $('#emailError2').hide();
     $('#passError').hide();
     $('#passError2').hide();
     $('#cPassError').hide();
+
+    //Email regex
     var re = new RegExp("^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$");
 
+    //Every time we release a key checks validation
     $('input').keyup(function () {
+        //Take all the values of inputs
         let name = document.getElementById('name').value
         let email = document.getElementById('email').value
         let password = document.getElementById('password').value
         let confirmpassword = document.getElementById('password-confirm').value
 
+        //Dont have any empty imput
         if (name === '') {
             $('#Registrarse').prop('disabled', true);
             $('#nameError').show();
@@ -31,6 +37,7 @@ $(document).ready(function () {
             $('#passError2').hide();
             $('#cPassError').hide();
         }
+        //Email regex validation
         else if (!re.test(email)){
             $('#Registrarse').prop('disabled', true);
             $('#emailError2').show();
@@ -49,6 +56,7 @@ $(document).ready(function () {
             $('#passError2').hide();
             $('#cPassError').hide();
         }
+        //Password length
         else if(password.length < 8){
             $('#Registrarse').prop('disabled', true);
             $('#passError2').show();
@@ -58,6 +66,7 @@ $(document).ready(function () {
             $('#passError').hide();
             $('#cPassError').hide();
         }
+        //Confirm password must be same as password
         else if(password !== confirmpassword){
             $('#Registrarse').prop('disabled', true);
             $('#cPassError').show();
