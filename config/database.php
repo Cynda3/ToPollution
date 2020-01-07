0439df3360dd854b38a4762a81c995f6026eec04
cynda3 <andergonzalez33@gmail.com>
2020-01-07 08:45:47 +0100

    Config changed

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
+            'charset' => 'utf8_unicode_ci',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
