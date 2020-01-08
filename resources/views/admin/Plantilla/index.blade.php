1e6e88ede8cb51bc87ed752df1211a53d50b8204
cynda3 <andergonzalez33@gmail.com>
2020-01-08 14:29:09 +0100

    Showing users on admin panel

--- resources/views/admin/Plantilla/index.blade.php
+++ resources/views/admin/Plantilla/index.blade.php
@@ -60,62 +60,23 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                                   <thead>
                                     <tr>
                                       <th>#</th>
-                                      <th>Project</th>
-                                      <th>Manager</th>                                   
-                                                                        
-                                      <th>Status</th>
-                                      <th>Progress</th>
+                                      <th>Name</th>
+                                      <th>Email</th>   
+                                      <th>Role</th>
+                                      <th>Delete</th>
                                   </tr>
                               </thead>
                               <tbody>
-                                <tr>
-                                  <td>1</td>
-                                  <td>Face book</td>
-                                  <td>Malorum</td>                                 
-                                                             
-                                  <td><span class="label label-danger">in progress</span></td>
-                                  <td><span class="badge badge-info">50%</span></td>
-                              </tr>
-                              <tr>
-                                  <td>2</td>
-                                  <td>Twitter</td>
-                                  <td>Evan</td>                               
-                                                                  
-                                  <td><span class="label label-success">completed</span></td>
-                                  <td><span class="badge badge-success">100%</span></td>
-                              </tr>
-                              <tr>
-                                  <td>3</td>
-                                  <td>Google</td>
-                                  <td>John</td>                                
-                                  
-                                  <td><span class="label label-warning">in progress</span></td>
-                                  <td><span class="badge badge-warning">75%</span></td>
-                              </tr>
-                              <tr>
-                                  <td>4</td>
-                                  <td>LinkedIn</td>
-                                  <td>Danial</td>                                 
-                                                             
-                                  <td><span class="label label-info">in progress</span></td>
-                                  <td><span class="badge badge-info">65%</span></td>
-                              </tr>
-                              <tr>
-                                  <td>5</td>
-                                  <td>Tumblr</td>
-                                  <td>David</td>                                
-                                                                 
-                                  <td><span class="label label-warning">in progress</span></td>
-                                  <td><span class="badge badge-danger">95%</span></td>
-                              </tr>
-                              <tr>
-                                  <td>6</td>
-                                  <td>Tesla</td>
-                                  <td>Mickey</td>                                  
-                                                             
-                                  <td><span class="label label-info">in progress</span></td>
-                                  <td><span class="badge badge-success">95%</span></td>
-                              </tr>
+                              	@foreach($users as $user)
+	                                <tr>
+	                                  <td>{{$user->id}}</td>
+	                                  <td>{{$user->name}}</td>
+	                                  <td>{{$user->email}}</td>                                 
+	                                                             
+	                                  <td><span class="label label-success">{{$user->role->name}}</span></td>
+	                                  <td><span class="label label-danger">Delete</span></td>
+	                                </tr>
+                                @endforeach
                           </tbody>
                       </table>
                   </div>
