db8601313c95abe7b526fd9a5d726f201aa3afe4
AsiFernandez <ajuanenama@zubirimanteo.com>
2020-01-10 10:23:24 +0100

    Links to Device Show added to the table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
                 <th scope="col">Latitude</th>
                 <th scope="col">Longitude</th>
                 <th scope="col">Owner</th>
+                <th scope="col" class="d-flex justify-content-center">Información</th>
             </tr>
         </thead>
         @foreach ($devices as $d)
@@ -19,6 +20,7 @@
                 <td>{{$d->latitude}}</td>
                 <td>{{$d->longitude}}</td>
                 <td>{{$d->user->name}}</td>
+                <td class="d-flex justify-content-center"><a href="{{route('devices.show',$d->id)}}"><i class="fas fa-glasses" style="color:white"></i></a></td>
             </tr>
         </tbody>
         @endforeach
