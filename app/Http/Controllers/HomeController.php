845b90144204b65196d10407669c9b7f1c9bc632
AsiFernandez <ajuanenama@zubirimanteo.com>
2020-01-10 09:48:57 +0100

    User panel show all the devices done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ namespace App\Http\Controllers;
   
 
 use Illuminate\Http\Request;
-
+use App\Device;
   
 
 class HomeController extends Controller
@@ -47,8 +47,8 @@ class HomeController extends Controller
     public function index()
 
     {
-
-        return view('home');
+        $devices = Device::all();
+        return view('home')->with('devices', $devices);
 
     }
 
