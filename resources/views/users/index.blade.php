02cb997e74445b612175d6ef6cb55779acd4f3b8
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2020-01-14 11:30:01 +0100

    Formating code again

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-<table class="table table-hover table-dark">
+    <table class="table table-hover table-dark">
         <thead>
             <tr>
                 <th scope="col">Id</th>
@@ -21,9 +21,9 @@
                 <td>{{$d->longitude}}</td>
                 <td>{{$d->user->name}}</td>
                 <td>
-                <!--<a href="{{route('devices.show',$d->id)}}"><i class="far fa-plus-square"></i></a> -->
-                <a href="{{route('devices.edit',$d->id)}}"><i class="fas fa-pencil-alt"></i></a>
-                <a href="{{route('devices.show',$d->id)}}"><i class="far fa-trash-alt"></i></a></td>
+                    <!--<a href="{{route('devices.show',$d->id)}}"><i class="far fa-plus-square"></i></a> -->
+                    <a href="{{route('devices.edit',$d->id)}}"><i class="fas fa-pencil-alt"></i></a>
+                    <a href="{{route('devices.show',$d->id)}}"><i class="far fa-trash-alt"></i></a></td>
             </tr>
         </tbody>
         @endforeach
