2e7049e1eb852bc7fda5560bdd6ade73aa61e998
AsiFernandez <ajuanenama@zubirimanteo.com>
2020-02-06 11:18:04 +0100

    size of the image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,7 +35,7 @@
     var greenIcon = L.icon({
     iconUrl: 'img/icon.png',
 
-    iconSize:     [38, 95], // size of the icon
+    iconSize:     [60, 95], // size of the icon
     iconAnchor:   [browserLong, browserLat], // point of the icon which will correspond to marker's location
     });
     marker_actual = L.marker([browserLat,browserLong], {icon: greenIcon}).addTo(map);
