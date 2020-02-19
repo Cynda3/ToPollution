d8862e771cbdef1cff97fc92d045a5ced104983f
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2020-02-19 10:30:15 +0100

    More tries with map icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,19 @@
   crossorigin="" />
 @endsection
 @section('content')
-<div id="mapid" style="height: 50rem;"></div>
+<div id="mapid"  style="height: 50rem;"></div>
 
-@endsection
-@section('scripts')
-<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+    @endsection
+    @section('scripts')
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
   crossorigin=""></script>
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/leaflet.locatecontrol/0.60.0/L.Control.Locate.min.css" />
-<script src="https://cdn.jsdelivr.net/leaflet.locatecontrol/0.60.0/L.Control.Locate.min.js" charset="utf-8"></script>
-<!-- -------MAPA------ -->
+  <script src="https://cdn.jsdelivr.net/leaflet.locatecontrol/0.60.0/L.Control.Locate.min.js" charset="utf-8"></script>
+    <!-- -------MAPA------ -->
 <script type="text/javascript">
+
   var map = L.map('mapid', {
      center: [ 43.31656, -1.987495 ],
      zoom: 14   
@@ -34,15 +35,12 @@
     
     //marker_actual = L.marker([browserLat,browserLong]).addTo(map);
     var iconPersona = L.icon({
-      iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-      
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41]
-    });
+      iconUrl: "/images/iconoPersona.png",
 
+      iconSize:     [38, 95], // size of the icon
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
     L.marker([browserLat,browserLong], {icon: iconPersona}).addTo(map)
     marker_actual.bindPopup('<b>Hola </b><br>Tu estas aqui').openPopup();
     map.setView([browserLat,browserLong], 18);  
